a0a84d3cd31f3adfad4ed5388374e9558f60dbbd
valeriapaulinalustres <valeriapaulinalustres@yahoo.com>
2022-03-24 08:26:05 -0300

    desafío clase 5

---
+++ src/components/ItemCount.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import './ItemCount.css'
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+
+    //destructuración 
+    const [counter, setCounter] = useState(initial);
+
+    //evento del botón de suma
+    const increase = () => {
+        counter < stock
+            ? setCounter(counter + 1)
+            : alert("no hay suficiente stock");
+    }
+
+    //evento del botón de resta
+    const decrease = () => {
+        counter > initial
+            ? setCounter(counter - 1)
+            : alert("compra mínima permitida: " + initial);
+    }
+
+    //evento del botón "agregar al carro" que ejecuta la acción onAdd enviada como prop por su padre
+    const handleOnAdd = () => {
+        onAdd(counter);
+    }
+
+    return (
+        <div className="countContainer">
+            <h1>arbolito</h1>
+            <div className="countButtons">
+                <button className="button" onClick={decrease}>➖</button>
+                <span className="value">{counter}</span>
+                <button className="button" onClick={increase}>➕</button>
+            </div>
+            <button onClick={handleOnAdd} className="countAdd button" >agregar al carrito 🛒</button>
+        </div>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,18 @@
+import ItemCount from './ItemCount.js'
+
 
 export function ItemListContainer({ greeting }) {
 
+    //función que pasa a su hijo ItemCount para el evento del botón "agregar al carrito"
+    const onAdd = (counter) => {
+        console.log(counter);
+    }
 
     return (
         <div>
             <h2>Bienvenido {greeting}</h2>
             <p>Aquí irá el Catálogo</p>
+            <ItemCount stock={5} initial={1} onAdd={onAdd} />
         </div>
 
     )
